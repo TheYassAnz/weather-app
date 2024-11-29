f57507828825c9230f325504aad4f202b4ad6e07
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-11-29 13:03:09 +0100

    feat: init react-native-paper

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Menu } from "react-native-paper";
 
 export default function Index() {
     return (
@@ -9,6 +10,11 @@ export default function Index() {
                 alignItems: "center",
             }}>
             <Text>Hello World!</Text>
+            <Menu.Item leadingIcon="redo" onPress={() => {}} title="Redo" />
+            <Menu.Item leadingIcon="undo" onPress={() => {}} title="Undo" />
+            <Menu.Item leadingIcon="content-cut" onPress={() => {}} title="Cut" disabled />
+            <Menu.Item leadingIcon="content-copy" onPress={() => {}} title="Copy" disabled />
+            <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Paste" />
         </View>
     );
 }
