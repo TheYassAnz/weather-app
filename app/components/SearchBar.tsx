c6cb2ca2ef348be896084d7cdb346d47d047bc03
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-11-29 16:09:18 +0100

    feat: component searchBar

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,61 @@
+import { Searchbar, List, Text, Icon } from "react-native-paper";
+import { useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+
+export default function SearchBar() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [cities, setCities] = useState([]);
+    useEffect(() => {
+        const fetchCityGeoCode = async (city: string) => {
+            try {
+                const response = await fetch(
+                    `https://nominatim.openstreetmap.org/search?q=${city}&format=json&limit=10`,
+                    {
+                        headers: {
+                            "User-Agent":
+                                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+                        },
+                    }
+                );
+                const data = await response.json();
+                setCities(data);
+            } catch (error) {
+                console.error("Error fetching city geocode:", error);
+                setCities([]);
+            }
+        };
+        fetchCityGeoCode(searchQuery);
+    }, [searchQuery]);
+    return (
+        <View style={styles.container}>
+            <Searchbar
+                placeholder="Search for a city..."
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+                selectionColor={"#000"}
+            />
+            {cities &&
+                cities.map((city: any) => {
+                    return (
+                        <View>
+                            <List.Item
+                                onPress={() => {
+                                    setSearchQuery("");
+                                    console.log(city);
+                                }}
+                                title={city.display_name}
+                                description={`Latitude: ${city.lat}, Longitude: ${city.lon}`}
+                            />
+                        </View>
+                    );
+                })}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+    },
+});
